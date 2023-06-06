5aeb5a5aded1da3945855f7dba752bb3c869bc54
JanMedocs <61686304+JanMedocs@users.noreply.github.com>
2023-06-06 11:04:11 +0200

    Initial commit

---
+++ src/app/aufgaben/aufgabe4/aufgabe4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aufgabe4',
+  templateUrl: './aufgabe4.component.html',
+  styleUrls: ['./aufgabe4.component.css']
+})
+export class Aufgabe4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
